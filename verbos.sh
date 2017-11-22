de62f56d88522c01ef62ade9c65fb5768843fdfb
Pacome BERNIER <pbernier@e3r11p23.42.fr>
2017-11-22 23:23:08 +0100

    [pbernier] - flag d working

--- verbos.sh
+++ verbos.sh
@@ -1,11 +1,12 @@
 
-asm_name="./asm"
+asm_name="asm"
 file_test="tests/verbos/"
+dir_list=("comment" "diverse" "instruction" "name")
 
 #d = directory
 flag_list="d"
-all_tests=$(ls -R ${file_test}*/*.s)
-nb_tests="$(echo "$(ls ${file_test}*/*.s | wc -l)" | tr -d ' ')"
+all_tests=()
+dir_invalid=()
 nb_done=0;
 
 underline="\033[4m"
@@ -14,31 +15,54 @@ grey="\033[38;5;8m"
 
 if [[ $1 =~ ^-[${flag_list}]+$ ]]; then
 	if [[ $1 =~ [d] ]]; then
-		unset ${*}
-		for i in ${*}
-		do
-			printf "ok $i\n"
-		done
-
+		dir_list=( "$@" )
+		unset dir_list[0]
+		if [[ -z "${dir_list[@]}" ]]; then
+			printf "usage: $0 -d [$(ls -m tests/verbos/)]\n"
+			exit
+		fi
 	fi
-else if [ $1 ]; then
-	printf "usage: $0 [-${flag_list}] [file ...]\n"
+	else if [ $1 ]; then
+		printf "usage: $0 [-${flag_list}] [file ...]\n"
+		exit
+	fi
+fi
+
+for dir in ${dir_list[@]}; do
+	valid="dont exist"
+	for dir_exist in $(echo "$(ls tests/verbos/)"); do
+		if [ ${dir} == ${dir_exist} ]; then
+			valid="exist"
+		fi
+	done
+	if [[ ${valid} = "dont exist" ]]; then
+		dir_invalid[${#dir_invalid[@]}]=${dir}
+	else
+		dir_tests=$(ls -R ${file_test}${dir}/*.s)
+		for test in ${dir_tests[@]}; do
+			all_tests[${#all_tests[@]}]=${test}
+		done
+	fi
+done
+
+if [ -n "${dir_invalid}" ]; then
+	for dir in ${dir_invalid[@]}; do
+		printf "$0: ${dir}: No such directory\n"
+	done
 	exit
-fi fi
+fi
 
+nb_tests=${#all_tests[@]}
 
-
-exit
 make asm
 
-for name in ${all_tests}
-do
+for name in ${all_tests[@]}; do
 	((nb_done++))
 	printf "\n[${nb_done}/${nb_tests}] "
 	printf "${underline}???"
 	printf "${reset}: "
 	printf "${grey}[$(basename ${name})]${reset}\n"
-
 	./${asm_name} ${name}
+	# printf "${asm_name} ${name}"
 	read
 done
