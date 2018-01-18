431dec523900dbf1da708ffa74b8275885808d77
Pacome BERNIER <pbernier@e2r8p20.42.fr>
2018-01-18 03:25:54 +0100

    [pbernier] - Battle ready to test

--- tests/champ_opti.sh
+++ tests/champ_opti.sh
@@ -1,12 +1,11 @@
-max=100
-# rm -f aaaaaa$(echo $((${max}+1))).s
-cp aaaaaa.s aaaaaa0.s
+max=4096
+cp ../aaaaaa.s ../aaaaaa0.s
 
-for i in $(seq 0 1000); do
-./battle.sh -c aaaaaa${i}.s
+for i in $(seq 0 ${max}); do
+./battle.sh -c ../aaaaaa${i}.s
 next=$(echo $((${i}+1)))
-sed "/live/ s/${i}/${next}/g" aaaaaa${i}.s > aaaaaa${i}.tmp
+sed "/live/ s/${i}/${next}/g" ../aaaaaa${i}.s > ../aaaaaa${i}.tmp
 
-mv aaaaaa${i}.tmp aaaaaa${next}.s
-rm aaaaaa${i}.s aaaaaa${next}.cor
+mv ../aaaaaa${i}.tmp ../aaaaaa${next}.s
+rm ../aaaaaa${i}.s ../aaaaaa${next}.cor
 done
