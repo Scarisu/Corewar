2f5f16780254f3bd16d21660fb94e0e1e4ac3634
Romain LECART <rlecart@e3r4p1.42.fr>
2018-01-24 12:00:32 +0100

    [rlecart]  - Better st && ld functions.

--- srcs/vm/opcodes/op_st.c
+++ srcs/vm/opcodes/op_st.c
@@ -6,7 +6,7 @@
 /*   By: rlecart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 21:45:27 by rlecart           #+#    #+#             */
-/*   Updated: 2018/01/24 10:01:13 by rlecart          ###   ########.fr       */
+/*   Updated: 2018/01/24 11:58:54 by rlecart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,31 @@ void	op_st(t_champ *champs, t_corewar *d, t_reg *reg)
 	int		r1;
 	int		r2;
 	int		ind;
-	int		pc2;
+	int		pc;
 	t_ocp	ocp;
 
 	(void)champs;
 	if (++reg->cycle == 5)
 	{
-		(pc2 = reg->pc + 2) > MEM_SIZE ? pc2 -= MEM_SIZE : pc2;
-		r1 = d->map[pc2] - 1;
+		(pc = reg->pc + 2) >= MEM_SIZE ? pc -= MEM_SIZE : pc;
+		r1 = d->map[pc] - 1;
 		if ((ocp = find_ocp(d->map[reg->pc + 1])).p[1] == O_REG)
 		{
-			(pc2 = reg->pc + 3) > MEM_SIZE ? pc2 -= MEM_SIZE : pc2;
-			r2 = d->map[pc2] - 1;
+			(pc = reg->pc + 3) >= MEM_SIZE ? pc -= MEM_SIZE : pc;
+			r2 = d->map[pc] - 1;
 			if (r1 >= 0 && r1 <= 16 && r2 >= 0 && r2 <= 16)
 				memcpy(reg->r[r2], reg->r[r1], 2);
 			jump_to_next(d, reg, 4);
 		}
 		else if (ocp.p[1] == O_IND)
 		{
-			(pc2 = reg->pc + 3) > MEM_SIZE ? pc2 -= MEM_SIZE : pc2;
+			(pc = reg->pc + 3) >= MEM_SIZE ? pc -= MEM_SIZE : pc;
 			ind = d->map[reg->pc + 3];
-			(pc2 = reg->pc + 4) > MEM_SIZE ? pc2 -= MEM_SIZE : pc2;
-			ind += d->map[pc2];
-			(pc2 = reg->pc + (ind * IDX_MOD) + 1) > MEM_SIZE ?
-				pc2 -= MEM_SIZE : pc2;
-			ft_memcpy(&d->map[pc2], reg->r[r1], 2);
+			(pc = reg->pc + 4) >= MEM_SIZE ? pc -= MEM_SIZE : pc;
+			ind += d->map[pc];
+			(pc = reg->pc + (ind * IDX_MOD) + 1) >= MEM_SIZE ?
+				pc -= MEM_SIZE : pc;
+			ft_memcpy(&d->map[pc], reg->r[r1], 2);
 			jump_to_next(d, reg, 5);
 		}
 		reg->cycle = 0;
