a37ea3820e57eff58a40392d9d31ccacff2b528e
Romain LECART <rlecart@e1r9p20.42.fr>
2018-02-26 23:52:57 +0100

    [rlecart]  - put hexa fix.

--- srcs/vm/utils2.c
+++ srcs/vm/utils2.c
@@ -6,7 +6,7 @@
 /*   By: rlecart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 04:14:13 by rlecart           #+#    #+#             */
-/*   Updated: 2018/02/25 19:31:57 by rlecart          ###   ########.fr       */
+/*   Updated: 2018/02/26 23:52:43 by rlecart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,8 @@ void	put_hexa(t_corewar *d, int nbc, int pc, int value)
 	}
 	while (++i < len)
 	{
-		d->map[i] = value >> 8 * (len - i - 1);
+		d->map[pc] = value >> 8 * (len - i - 1);
 		d->colors[pc] = nbc;
 		(++pc) >= MEM_SIZE ? pc -= MEM_SIZE : pc;
 	}
-	//printw("pc = %d\n", pc);
-	//refresh();
-	//while (1);
 }
